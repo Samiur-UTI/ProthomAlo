3e4dcf60baf382f8af56b762385fb71bbcf56dea
Ruimas-UTI <allpraisethelord@gmail.com>
2021-04-21 02:57:32 +0600

    Bismillahir Rahmanir Rahim

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+//import axios from 'axios';
 export default function App() {
+    async function data (){
+        const response = await fetch('./data.json');
+        const data = await response.json();
+    }
+    data();
     return (
-        <div>
-            Prothomalo
+        <div className="main">
+            <div className="logo"></div>
+            <div className="content"></div>
+            <div className="footer"></div>            
         </div>
     )
 }
