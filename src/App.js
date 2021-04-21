65b4411dcfec8b54cbc738bbeb6ca1d3a006c1d8
Ruimas-UTI <allpraisethelord@gmail.com>
2021-04-21 13:12:47 +0600

    grid midway

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import NewsItem from './components/news-item/newsItem.component';
 import './App.scss';
 export default function App() {
-    async function data (){
-        const response = await fetch('./data.json');
-        const data = await response.json();
+    const [data,setData] = useState([]);
+    async function bringData (){
+        try {
+            const response = await fetch('./data.json');
+            const data = await response.json();
+            const value = await data;
+            return value;
+        } catch (error) {
+            console.error(error)
+        }
     }
-    data();
+    useEffect(()=>{
+       const changeData = async () => {
+            const response = await bringData();
+            setData(response)
+        };
+       changeData();
+    },[]);
     return (
         <div className='root'>
             <div className="logo"></div>
@@ -21,21 +35,11 @@ export default function App() {
 
                     </div>
                     <div className='news-item'>
-                        <div className='news-general'>
-
-                        </div>
-                        <div className='news-general'>
-
-                        </div>
-                        <div className='news-general'>
-
-                        </div>
-                    </div>
-                    <div className='news-item'>
-
-                    </div>
-                    <div className='news-item'>
-
+                        {data.slice(0,9).map(item => (
+                             <div className='news-general'>
+                                 <NewsItem {...item}/>
+                             </div>
+                        ))}
                     </div>
                 </div>
                 <div className='content-item-add'>
---
+++ src/components/news-item/newsItem.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NewsItem(props) {
+    console.log(props)
+    return (
+        <div>
+            NewsItem
+        </div>
+    )
+}
