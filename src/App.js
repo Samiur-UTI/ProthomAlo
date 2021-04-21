0dfa5a6c959353b1141f991828b1145634569591
Ruimas-UTI <allpraisethelord@gmail.com>
2021-04-21 10:36:03 +0600

    grid scaling init

--- src/App.js
+++ src/App.js
@@ -9,7 +9,39 @@ export default function App() {
     return (
         <div className='root'>
             <div className="logo"></div>
-            <div className="content"></div>
+            <div className="content">
+                <div className='content-item-news'>
+                    <div className='news-item-important'>
+                        <div className='breaking-news'>
+
+                        </div>
+                        <div className='notso-breaking-news'>
+
+                        </div>
+
+                    </div>
+                    <div className='news-item'>
+                        <div className='news-general'>
+
+                        </div>
+                        <div className='news-general'>
+
+                        </div>
+                        <div className='news-general'>
+
+                        </div>
+                    </div>
+                    <div className='news-item'>
+
+                    </div>
+                    <div className='news-item'>
+
+                    </div>
+                </div>
+                <div className='content-item-add'>
+
+                </div>
+            </div>
             <div className="footer"></div>            
         </div>
     )
