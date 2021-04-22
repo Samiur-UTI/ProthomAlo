78690bbc06cb80113c505ee90a71c36fd2117c5d
Ruimas-UTI <allpraisethelord@gmail.com>
2021-04-23 03:12:38 +0600

    material init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import NewsItem from './components/news-item/newsItem.component';
+import News from './components/news-item/news.component';
 import './App.scss';
 export default function App() {
     const [data,setData] = useState([]);
@@ -23,29 +23,9 @@ export default function App() {
     return (
         <div className='root'>
             <div className="logo"></div>
-            <div className="content">
-                <div className='content-item-news'>
-                    <div className='news-item-important'>
-                        <div className='breaking-news'>
-
-                        </div>
-                        <div className='notso-breaking-news'>
-
-                        </div>
-
-                    </div>
-                    <div className='news-item'>
-                        {data.slice(0,9).map(item => (
-                             <div className='news-general'>
-                                 <NewsItem {...item}/>
-                             </div>
-                        ))}
-                    </div>
+                <div className="content">
+                    <News data={data}/>
                 </div>
-                <div className='content-item-add'>
-
-                </div>
-            </div>
             <div className="footer"></div>            
         </div>
     )
